15de2db895d6dcccc6f95554ac2e78ba05a0e910
Oleg Arshinskii <velet5@ya.ru>
2022-05-15 21:41:45 +0300

    add monaco-editor

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import "./style.css";
+import * as monaco from "monaco-editor";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+
+const w: any = self;
+
+w.MonacoEnvironment = {
+  getWorker(_: any, label: string) {
+    if (label === "json") {
+      return new jsonWorker();
+    }
+    if (label === "css" || label === "scss" || label === "less") {
+      return new cssWorker();
+    }
+    if (label === "html" || label === "handlebars" || label === "razor") {
+      return new htmlWorker();
+    }
+    if (label === "typescript" || label === "javascript") {
+      return new tsWorker();
+    }
+    return new editorWorker();
+  },
+};
+window.addEventListener("DOMContentLoaded", () => {
+  const element = document.getElementById("editor");
+
+  const editor = monaco.editor.create(element!, {
+    language: "typescript",
+  });
+
+  console.log(editor);
+});
